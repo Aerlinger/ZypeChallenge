5e96a0c72f92c3b17302b3d06261177b2da04dda
Anthony Erlinger <aerlinger@gmail.com>
2017-03-01 11:18:33 -0500

    Strip project down to a barebones configuration (See commit notes)
    We only need ActionController and ActionView for now. Most gems
    unrelated to ActionController and ActionView have been removed.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,18 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+
+%w(
+  action_mailer/railtie
+  action_controller/railtie
+  action_view/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
