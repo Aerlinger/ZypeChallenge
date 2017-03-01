2261d08e4d289a63e8ca1100969b7fa95f46e8ae
Anthony Erlinger <aerlinger@gmail.com>
2017-03-01 11:25:59 -0500

    Add routes, controllers, and views for videos and session resource

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < ApplicationController
+  # Display login page
+  def new
+  end
+
+  # Persist session and redirect to video
+  def create
+  end
+end
---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,9 @@
+class VideosController < ApplicationController
+  # Display all videos
+  def index
+  end
+
+  # Display individual video
+  def show
+  end
+end
